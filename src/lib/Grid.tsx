2614c10a56eca71145095e2a1907faf2fdf22dbc
Murray Jack <murrayjack@gmail.com>
2022-02-07 17:44:39 +1100

    more mistakes

--- src/lib/Grid.tsx
+++ src/lib/Grid.tsx
@@ -15,10 +15,8 @@ export const Grid = () => {
   `);
 
   return (
-    <table>
-      <thead>
-        {/* <!-- todo --> */}
-      </thead>
+    <table border="1">
+      <thead>{/* <!-- todo --> */}</thead>
       <tbody>
         {data?.authors?.map((e) => (
           <tr>
@@ -37,12 +35,7 @@ export const Grid = () => {
                   });
                 }}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
+                <svg width="24" height="24" viewBox="0 0 24 24">
                   <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
                 </svg>
               </button>
